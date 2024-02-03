d342b6a3bffae4ef6dd74fbc58989dadf3d93d56
NosokYT <100868283+Nosok2011@users.noreply.github.com>
2024-02-04 00:24:14 +0300

    1.1
    версия 1.1

--- artemiy.py
+++ artemiy.py
@@ -2,14 +2,19 @@
 from random import choice
 from jsonc import loads, dumps
 from fuzzywuzzy import fuzz
-settings = loads(open("settings.json", encoding="UTF-8").read())
+from datetime import datetime
+from geopy.geocoders import Nominatim
+from requests import get
+settings = loads(open("settings.jsonc", encoding="UTF-8").read())
+locator = Nominatim(user_agent="Yandex")
 def edit_settings(new):
-    settings_ = open("settings.json", "w", encoding="UTF-8")
-    comment = """// в этой директории существует файл \"settings_orig.json\".
-                 // он нужен для того, чтобы возвращаться к заводским настройкам.
-                 // нужно просто скопировать содержимое оригинального файла в этот.\n"""
-    comment = "\n".join(comment.split("\n"))
-    comment = comment.replace(" " * 17, "")
+    settings_ = open("settings.jsonc", "w", encoding="UTF-8")
+    comment = [
+        "// в этой директории существует файл \"settings_orig.jsonc\".",
+        "// он нужен для того, чтобы возвращаться к заводским настройкам.",
+        "// нужно просто скопировать содержимое оригинального файла в этот.\n"
+    ]
+    comment = "\n".join(comment)
     settings_.write(comment + dumps(new, indent=4, ensure_ascii=False))
     settings_.close()
     print("Внимание!! Были изменены настройки. Перезапустите ассистента для полного принятия изменений.")
@@ -25,16 +30,16 @@ def recog_cmd(cmd):
 def exec_cmd(cmd):
     if settings["settings"]["debug_mode"]:
         print(cmd)
-    from datetime import datetime # импортируем здесь, а не в начале, чтобы показывало текущее время и дату, а не на момент запуска
-    now = datetime.now()
     exec_msg = choice(settings["answers"]["executing"])
     done_msg = choice(settings["answers"]["done"])
     if cmd["ratio"] >= 50:
         match cmd["cmd_name"]:
             case "time":
+                now = datetime.now()
                 print(choice(settings["answers"]["time"]) % (now.hour, now.minute, now.second))
             case "date":
-                print(choice(settings["answers"]["date"]) % (now.day, now.month, now.year))
+                today = datetime.today()
+                print(choice(settings["answers"]["date"]) % (today.day, today.month, today.year))
             case "disable_welcome_message":
                 print(exec_msg)
                 settings["settings"]["welcome_message_enabled"] = False
@@ -49,7 +54,35 @@ def exec_cmd(cmd):
                 print(choice(settings["answers"]["exit"]))
                 exit()
             case "help":
-                print(choice(settings["answers"]["help"]))
+                print(f"{choice(settings["answers"]["help"])} (полный список см. по ссылке https://clck.ru/38Xg5s в разделе \"Возможности\"):")
+                help_ = [
+                    "1. Узнавать время",
+                    "2. Узнавать дату",
+                    "3. Узнавать погоду"
+                ]
+                help_ = "\n".join(help_)
+                print(help_)
+            case "weather":
+                addr = input("Введите адрес, где хотите узнать погоду: ")
+                location = locator.geocode(addr)
+                addr_ = location.address
+                lat, lon = location.latitude, location.longitude
+                api = settings["settings"]["weather_api_key"]
+                if api == 0:
+                    api = input("Введите ключ API для api.weatherapi.com: ")
+                    settings["settings"]["weather_api_key"] = api
+                    edit_settings(settings)
+                    print("Ключ сохранён.")
+                req = get(f"http://api.weatherapi.com/v1/current.json?key={api}&q={lat},{lon}&lang=ru").json()
+                cur = req["current"]
+                print(choice(settings["answers"]["weather"]) % addr_ + ":")
+                print(f"Температура: {int(cur["temp_c"])}°C")
+                print(f"Ощущается как: {int(cur["feelslike_c"])}°C")
+                print(f"Описание: {cur["condition"]["text"]}")
+                print(f"Скорость ветра: {int(cur["wind_kph"] * 1000 / 3600)} м/с")
+                print(f"Давление: {int(cur["pressure_mb"] * 0.750063755419211)} мм рт. ст.")
+                print(f"Влажность: {cur["humidity"]}%")
+                print(f"Облачность: {cur["cloud"]}%")
             case _:
                 print(choice(settings["answers"]["unknown"]))
     else:
