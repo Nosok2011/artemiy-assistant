9a2e986c574953a3669ce765158b56d2f90827fa
NosokYT <100868283+Nosok2011@users.noreply.github.com>
2024-02-04 23:36:54 +0300

    1.2
    версия 1.2

--- artemiy.py
+++ artemiy.py
@@ -5,8 +5,21 @@ from fuzzywuzzy import fuzz
 from datetime import datetime
 from geopy.geocoders import Nominatim
 from requests import get
+from sys import exit
+from currencyapicom import Client
+from math import floor
 settings = loads(open("settings.jsonc", encoding="UTF-8").read())
-locator = Nominatim(user_agent="Yandex")
+def round_1(num):
+    num_ = (num * 10) % 10
+    num = floor(num)
+    num += 1 if num_ >= 5 else 0
+    return num
+def get_api_key(for_, setting_name):
+    api = input(f"Введите ключ API для {for_}: ")
+    settings["settings"][setting_name] = api
+    edit_settings(settings)
+    print("Ключ сохранён.")
+    return api
 def edit_settings(new):
     settings_ = open("settings.jsonc", "w", encoding="UTF-8")
     comment = [
@@ -28,8 +41,7 @@ def recog_cmd(cmd):
                 pars["ratio"] = ratio
     return pars
 def exec_cmd(cmd):
-    if settings["settings"]["debug_mode"]:
-        print(cmd)
+    print(cmd) if settings["settings"]["debug_mode"] else 0
     exec_msg = choice(settings["answers"]["executing"])
     done_msg = choice(settings["answers"]["done"])
     if cmd["ratio"] >= 50:
@@ -45,11 +57,13 @@ def exec_cmd(cmd):
                 settings["settings"]["welcome_message_enabled"] = False
                 edit_settings(settings)
                 print(done_msg)
+                print(choice(settings["answers"]["disable_welcome_message"]))
             case "enable_welcome_message":
                 print(exec_msg)
                 settings["settings"]["welcome_message_enabled"] = True
                 edit_settings(settings)
                 print(done_msg)
+                print(choice(settings["answers"]["enable_welcome_message"]))
             case "exit":
                 print(choice(settings["answers"]["exit"]))
                 exit()
@@ -58,51 +72,65 @@ def exec_cmd(cmd):
                 help_ = [
                     "1. Узнавать время",
                     "2. Узнавать дату",
-                    "3. Узнавать погоду"
+                    "3. Узнавать погоду",
+                    "4. Узнавать курс рубля",
+                    "5. Подбрасывать монетку"
                 ]
                 help_ = "\n".join(help_)
                 print(help_)
             case "weather":
                 addr = input("Введите адрес, где хотите узнать погоду: ")
+                locator = Nominatim(user_agent="Yandex")
                 location = locator.geocode(addr)
                 addr_ = location.address
                 lat, lon = location.latitude, location.longitude
-                api = settings["settings"]["weather_api_key"]
-                if api == 0:
-                    api = input("Введите ключ API для api.weatherapi.com: ")
-                    settings["settings"]["weather_api_key"] = api
-                    edit_settings(settings)
-                    print("Ключ сохранён.")
-                req = get(f"http://api.weatherapi.com/v1/current.json?key={api}&q={lat},{lon}&lang=ru").json()
-                cur = req["current"]
-                print(choice(settings["answers"]["weather"]) % addr_ + ":")
-                print(f"Температура: {int(cur["temp_c"])}°C")
-                print(f"Ощущается как: {int(cur["feelslike_c"])}°C")
-                print(f"Описание: {cur["condition"]["text"]}")
-                print(f"Скорость ветра: {int(cur["wind_kph"] * 1000 / 3600)} м/с")
-                print(f"Давление: {int(cur["pressure_mb"] * 0.750063755419211)} мм рт. ст.")
-                print(f"Влажность: {cur["humidity"]}%")
-                print(f"Облачность: {cur["cloud"]}%")
+                w_api = settings["settings"]["weather_api_key"]
+                w_api = get_api_key("weatherapi.com", "weather_api_key") if w_api == 0 else w_api
+                req = get(f"http://api.weatherapi.com/v1/current.json?key={w_api}&q={lat},{lon}&lang=ru").json()
+                if "error" in req.keys():
+                    err = req["error"]
+                    print("Ошибка выполнения запроса.")
+                    print(f"Код ошибки: {err["code"]}")
+                    print(f"Описание ошибки: {err["message"]}")
+                else:
+                    cur = req["current"]
+                    print(choice(settings["answers"]["weather"]) % addr_ + ":")
+                    print(f"Температура: {int(cur["temp_c"])}°C")
+                    print(f"Ощущается как: {int(cur["feelslike_c"])}°C")
+                    print(f"Описание: {cur["condition"]["text"]}")
+                    print(f"Скорость ветра: {int(cur["wind_kph"] * 1000 / 3600)} м/с")
+                    print(f"Давление: {int(cur["pressure_mb"] * 0.750063755419211)} мм рт. ст.")
+                    print(f"Влажность: {cur["humidity"]}%")
+                    print(f"Облачность: {cur["cloud"]}%")
+            case "currency":
+                c_api = settings["settings"]["currency_api_key"]
+                c_api = get_api_key("currencyapi.com", "currency_api_key") if c_api == 0 else c_api
+                client = Client(c_api)
+                usd = client.latest("USD", ["RUB"])["data"]["RUB"]["value"]
+                eur = client.latest("EUR", ["RUB"])["data"]["RUB"]["value"]
+                print(f"1$ = {round_1(usd)}₽")
+                print(f"1€ = {round_1(eur)}₽")
+            case "flip_coin":
+                coin = ["орёл", "решка"]
+                rnd = choice(coin)
+                print(f"{rnd.capitalize()}.")
             case _:
-                print(choice(settings["answers"]["unknown"]))
+                print(choice(settings["answers"]["not_implemented"]))
     else:
         match cmd["cmd"]:
-            case "debug_mode_on":
+            case "debug_mode":
                 print(exec_msg)
-                settings["settings"]["debug_mode"] = True
+                settings["settings"]["debug_mode"] = not settings["settings"]["debug_mode"]
                 edit_settings(settings)
                 print(done_msg)
-                print("Активирован режим отладки. В этом режиме перед выполнением каждой команды будут выводиться её данные в виде словаря python.")
-            case "debug_mode_off":
-                print(exec_msg)
-                settings["settings"]["debug_mode"] = False
-                edit_settings(settings)
-                print(done_msg)
-                print("Режим отладки деактивирован.")
+                print(
+                    "Активирован режим отладки. В этом режиме перед выполнением каждой команды будут выводиться её данные в виде словаря python."
+                    if settings["settings"]["debug_mode"]
+                    else "Режим отладки деактивирован."
+                )
             case _:
                 print(choice(settings["answers"]["unknown"]))
-if settings["settings"]["welcome_message_enabled"]:
-    print(choice(settings["answers"]["welcome"]))
+print(choice(settings["answers"]["welcome"])) if settings["settings"]["welcome_message_enabled"] else 0
 while True:
     query = input(">>> ")
     cmd = recog_cmd(query)
